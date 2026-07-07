bfb7eb6925442730e699f0a830e6ddef4cb9dc21
Inna Shishova <sh201608@yandex.ru>
2026-07-08 00:43:36 +0400

    fix selenoid capabilities

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -18,8 +18,11 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
 
         Configuration.browserCapabilities = capabilities;
 
