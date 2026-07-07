302c3d88d41f781750d00cda41f3a4a223c4f5c7
Inna Shishova <sh201608@yandex.ru>
2026-07-08 00:22:42 +0400

    add allure steps

--- src/test/java/pages/TextBoxPage.java
+++ src/test/java/pages/TextBoxPage.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import io.qameta.allure.Step;
 
 public class TextBoxPage {
 
@@ -17,43 +18,51 @@ public class TextBoxPage {
             checkCurrentAddress = $("#output #currentAddress"),
             checkPermanentAddress = $("#output #permanentAddress");
 
+    @Step("Открываем страницу Text Box")
     public TextBoxPage openPage() {
         open("/text-box");
         $(".text-center").shouldHave(text("Text Box"));
         return this;
     }
 
+    @Step("Удаляем баннеры")
     public TextBoxPage removeBanners() {
         executeJavaScript("document.querySelector('#fixedban')?.remove()");
         executeJavaScript("document.querySelector('footer')?.remove()");
         return this;
     }
 
+    @Step("Вводим имя: {value}")
     public TextBoxPage setFullName(String value) {
         userNameInput.setValue(value);
         return this;
     }
 
+    @Step("Вводим email: {value}")
     public TextBoxPage setEmail(String value) {
         userEmailInput.setValue(value);
         return this;
     }
 
+    @Step("Вводим текущий адрес: {value}")
     public TextBoxPage setCurrentAddress(String value) {
         currentAddressInput.setValue(value);
         return this;
     }
 
+    @Step("Вводим постоянный адрес: {value}")
     public TextBoxPage setPermanentAddress(String value) {
         permanentAddressInput.setValue(value);
         return this;
     }
 
+    @Step("Нажимаем кнопку Submit")
     public TextBoxPage submit() {
         submitButton.click();
         return this;
     }
 
+    @Step("Проверяем введенные данные")
     public TextBoxPage checkResult(String name,
                                    String email,
                                    String currentAddress,
